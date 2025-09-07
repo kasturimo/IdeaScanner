4c142470b81e0c5973ca0c6e26fe2decde51da0e
Kasturi Mondal <120777525+kasturimo@users.noreply.github.com>
2025-09-07 16:11:52 +0530

    Create Validator.kt

---
+++ app/src/main/java/com/ideascanner/Validator.kt
@@ -0,0 +1,11 @@
+package com.ideascanner.utils
+
+object Validator {
+    fun isValidEmail(email: String): Boolean {
+        return android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()
+    }
+
+    fun isValidPassword(password: String): Boolean {
+        return password.length >= 6
+    }
+}
